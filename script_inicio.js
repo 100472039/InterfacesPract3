6105bcef3e139946239dad6e089e19744c3969d4
100472037@alumnos.uc3m.es <Simpaticas_22>
2023-12-07 16:53:33 +0100

    calendario

--- script_inicio.js
+++ script_inicio.js
@@ -1,7 +1,3 @@
-function cerrarMenu() {
-    var btnMenu = document.getElementById('btn-menu');
-    btnMenu.checked = false;
-}
 function EnviarComen(){
     var comentario = document.getElementById("comentarios").value;
     
@@ -58,4 +54,107 @@ document.addEventListener('DOMContentLoaded', (event) => {
         slider.style.transform = `translate(-${operacion}%)`;
         slider.style.transition = "all ease .6s"       
     };  
+
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
+
+    let currentDate = new Date();
+    let currentDay = currentDate.getDate();
+    let monthNumber = currentDate.getMonth();
+    let currentYear = currentDate.getFullYear();
+    let selectedDayElement;
+
+    let dates = document.getElementById('dates');
+    let month = document.getElementById('month');
+    let year = document.getElementById('year');
+
+    let prevMonthDOM = document.getElementById('prev-month');
+    let nextMonthDOM = document.getElementById('next-month');
+
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+
+    prevMonthDOM.addEventListener('click', ()=>lastMonth());
+    nextMonthDOM.addEventListener('click', ()=>nextMonth());
+
+
+
+    const writeMonth = (month) => {
+
+        for(let i = startDay(); i>0;i--){
+            dates.innerHTML += ` <div class="calendar__date calendar__item calendar__last-days">
+                ${getTotalDays(monthNumber-1)-(i-1)}
+            </div>`;
+        }
+
+        for(let i=1; i<=getTotalDays(month); i++){
+            dates.innerHTML += ` <div class="calendar__date calendar__item" onclick="selectDay(this, ${i})">${i}</div>`;
+        }
+    }
+    
+    window.selectDay = (element, day) => {
+        selectedDay = day;
+        if(selectedDayElement) {
+            selectedDayElement.classList.remove('calendar__today');  // Restablecer el estilo del día anteriormente seleccionado
+        }
+        selectedDayElement = element;
+        selectedDayElement.classList.add('calendar__today');  // Cambiar el estilo del día seleccionado
+    }
+    const getTotalDays = month => {
+        if(month === -1) month = 11;
+
+        if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+            return  31;
+
+        } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+            return 30;
+
+        } else {
+
+            return isLeap() ? 29:28;
+        }
+    }
+
+    const isLeap = () => {
+        return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+    }
+
+    const startDay = () => {
+        let start = new Date(currentYear, monthNumber, 1);
+        return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1;
+    }
+
+    const lastMonth = () => {
+        if(monthNumber !== 0){
+            monthNumber--;
+        }else{
+            monthNumber = 11;
+            currentYear--;
+        }
+
+        setNewDate();
+    }
+
+    const nextMonth = () => {
+        if(monthNumber !== 11){
+            monthNumber++;
+        }else{
+            monthNumber = 0;
+            currentYear++;
+        }
+
+        setNewDate();
+    }
+
+    const setNewDate = () => {
+        currentDate.setFullYear(currentYear,monthNumber,currentDay);
+        month.textContent = monthNames[monthNumber];
+        year.textContent = currentYear.toString();
+        dates.textContent = '';
+        writeMonth(monthNumber);
+    }
+
+
+    writeMonth(monthNumber);
 });
